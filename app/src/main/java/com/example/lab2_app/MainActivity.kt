451c483048e76da7f74b905687a31ffeceefe5cf
Anton Solodov <sobaken334@gmail.com>
2026-05-05 19:57:40 +0300

    надеюсь всё зальётся

---
+++ app/src/main/java/com/example/lab2_app/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lab2_app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val editText = findViewById<EditText>(R.id.editTextText)
+        val button = findViewById<Button>(R.id.button)
+        textView.text = ""
+        button.setOnClickListener { textView.text = "Привет, " + editText.text + "!" }
+
+        if (!editText.text.isEmpty()) {
+            textView.text = "Привет, " + editText.text + "!"
+        } else {
+            textView.text = ""
+        }
+    }
+}
